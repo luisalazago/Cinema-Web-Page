cab300b8b274d777151aee1dced1db183c20ae4d
luisalazago <luisalazago@gmail.com>
2026-01-22 00:23:45 -0500

    [NEW] Create java project using Spring Boot, Maven and Java. The YAML file is updated with the DB connection. The (pom.xml) file is configurated with the dependencies used in the project. The project is running currently good

---
+++ back-end/cine-backend/src/main/java/com/cinedigital/cine_backend/CineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cinedigital.cine_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineBackendApplication.class, args);
+	}
+
+}
